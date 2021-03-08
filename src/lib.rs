85db17452fd3c8b5eae3873c84caf0f236f8382d
Eric Smith <paytonrules@gmail.com>
2021-03-08 07:57:07 -0600

    Render the (giant) sized RHB

--- src/lib.rs
+++ src/lib.rs
@@ -38,29 +38,38 @@ pub fn main_js() -> Result<(), JsValue> {
         5,
     );
 
-    image.set_onload(Some(onload.as_ref().unchecked_ref()));
-    image.set_src("Idle (1).png");
-
-    context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<bool>();
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
         let image = web_sys::HtmlImageElement::new().unwrap();
 
-        let onload = Closure::once(Box::new(move || {
-            success_tx
-                .send(true)
-                .expect("Could not send load event to receiver")
-        }) as Box<dyn FnOnce()>);
+        let callback = Closure::once(Box::new(move || {
+            success_tx.send(());
+        }));
 
-        image.set_onload(Some(onload.as_ref().unchecked_ref()));
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
         image.set_src("Idle (1).png");
 
         success_rx.await;
 
         context.draw_image_with_html_image_element(&image, 0.0, 0.0);
     });
+
+    /*
+        wasm_bindgen_futures::spawn_local(async move {
+            let (success_tx, success_rx) = futures::channel::oneshot::channel::<bool>();
+
+            let onload = Closure::once(Box::new(move || {
+                success_tx
+                    .send(true)
+                    .expect("Could not send load event to receiver")
+            }) as Box<dyn FnOnce()>);
+
+            image.set_onload(Some(onload.as_ref().unchecked_ref()));
+
+            success_rx.await;
+
+        });
+    */
 
     Ok(())
 }
