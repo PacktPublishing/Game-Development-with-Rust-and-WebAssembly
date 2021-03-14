d2df42874f1444b994f4f150896dfbc0f0fed22a
Eric Smith <paytonrules@gmail.com>
2021-03-14 15:32:21 -0500

    Get our friend RHB running.
    This completes chapter 2.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
+use std::collections::HashMap;
+
 use rand::prelude::*;
+use serde::Deserialize;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
+
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Deserialize)]
+struct Cell {
+    frame: Rect,
+}
+
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -31,14 +52,14 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        5,
-    );
-
     wasm_bindgen_futures::spawn_local(async move {
+        let json = fetch_json("rhb.json")
+            .await
+            .expect("Could not fetch rhb.json");
+        let sheet: Sheet = json
+            .into_serde()
+            .expect("Could not convert rhb.json into a Sheet structure");
+
         let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
         let image = web_sys::HtmlImageElement::new().unwrap();
 
@@ -47,94 +68,47 @@ pub fn main_js() -> Result<(), JsValue> {
         }));
 
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_src("Idle (1).png");
+        image.set_src("rhb.png");
 
         success_rx.await;
 
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+        let mut frame = -1;
+        let interval_callback = Closure::wrap(Box::new(move || {
+            frame = (frame + 1) % 8;
+            let frame_name = format!("Run ({}).png", frame + 1);
+            console::log_1(&JsValue::from_str(&frame_name));
+            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
+
+            context.clear_rect(0.0, 0.0, 600.0, 600.0);
+            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                sprite.frame.x.into(),
+                sprite.frame.y.into(),
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+                300.0,
+                300.0,
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+            );
+        }) as Box<dyn FnMut()>);
+
+        window.set_interval_with_callback_and_timeout_and_arguments_0(
+            interval_callback.as_ref().unchecked_ref(),
+            50,
+        );
+        interval_callback.forget();
     });
-
-    /*
-        wasm_bindgen_futures::spawn_local(async move {
-            let (success_tx, success_rx) = futures::channel::oneshot::channel::<bool>();
-
-            let onload = Closure::once(Box::new(move || {
-                success_tx
-                    .send(true)
-                    .expect("Could not send load event to receiver")
-            }) as Box<dyn FnOnce()>);
-
-            image.set_onload(Some(onload.as_ref().unchecked_ref()));
-
-            success_rx.await;
-
-        });
-    */
 
     Ok(())
 }
 
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(&context, points, color);
-    let depth = depth - 1;
+fn callback() {}
 
-    let [top, left, right] = points;
-    if depth > 0 {
-        let mut rng = thread_rng();
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
+    let resp: web_sys::Response = resp_value.dyn_into()?;
 
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-        let left_middle = midpoint(top, left);
-        let right_middle = midpoint(top, right);
-        let bottom_middle = midpoint(left, right);
-        sierpinski(
-            &context,
-            [top, left_middle, right_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [left_middle, left, bottom_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [right_middle, bottom_middle, right],
-            next_color,
-            depth,
-        );
-    }
-}
-
-fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
-}
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
