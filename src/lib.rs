d43cd842b15727f06b73e715bb016485edda5d9f
Eric Smith <paytonrules@gmail.com>
2021-04-12 07:51:41 -0500

    Extract load_image into the engine
    It's debatable whether load_image belongs in the engine or the browser.
    Load_json is in the browser, but maybe it belongs in engine.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use wasm_bindgen::JsCast;
 use web_sys::console;
 
 mod browser;
+mod engine;
 
 #[derive(Deserialize)]
 struct Rect {
@@ -41,27 +42,9 @@ pub fn main_js() -> Result<(), JsValue> {
             .into_serde()
             .expect("Could not convert rhb.json into a Sheet structure");
 
-        let image = browser::new_image().unwrap();
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let success_callback = browser::create_one_time_closure(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(()));
-            }
-        });
-
-        let error_callback = browser::create_one_time_closure_with_err(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                error_tx.send(Err(err));
-            }
-        });
-
-        image.set_onload(Some(success_callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("rhb.png");
-
-        success_rx.await;
+        let image = engine::load_image("rhb.png")
+            .await
+            .expect("Could not load rhb.png");
 
         let mut frame = -1;
         let interval_callback = Closure::wrap(Box::new(move || {
