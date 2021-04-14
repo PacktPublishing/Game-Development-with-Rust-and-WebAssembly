2fa571492b13c5a87acdd059619992a6c993bcf5
Eric Smith <paytonrules@gmail.com>
2021-04-14 07:41:50 -0500

    Start the game loop and see RHB run
    He is now super-fast again.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-use std::{collections::HashMap, rc::Rc, sync::Mutex};
-
+use engine::{Game, GameLoop};
 use serde::Deserialize;
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::console;
+use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
 #[macro_use]
 mod browser;
@@ -27,6 +26,36 @@ struct Sheet {
     frames: HashMap<String, Cell>,
 }
 
+struct WalkTheDog {
+    image: HtmlImageElement,
+    sheet: Sheet,
+    frame: u8,
+}
+
+impl Game for WalkTheDog {
+    fn update(&mut self) {
+        self.frame = (self.frame + 1) % 8;
+    }
+
+    fn draw(&self, context: CanvasRenderingContext2d) {
+        let frame_name = format!("Run ({}).png", self.frame + 1);
+        let sprite = self.sheet.frames.get(&frame_name).expect("Cell not found");
+
+        context.clear_rect(0.0, 0.0, 600.0, 600.0);
+        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+            &self.image,
+            sprite.frame.x.into(),
+            sprite.frame.y.into(),
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+            300.0,
+            300.0,
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+        );
+    }
+}
+
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -47,34 +76,13 @@ pub fn main_js() -> Result<(), JsValue> {
             .await
             .expect("Could not load rhb.png");
 
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            log!("Run ({}).png", frame + 1);
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
+        let game = WalkTheDog {
+            image,
+            sheet,
+            frame: 0,
+        };
 
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                sprite.frame.x.into(),
-                sprite.frame.y.into(),
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-                300.0,
-                300.0,
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-            );
-        }) as Box<dyn FnMut()>);
-
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                interval_callback.as_ref().unchecked_ref(),
-                50,
-            );
-        interval_callback.forget();
+        GameLoop::start(game).expect("Could not start game loop");
     });
 
     Ok(())
