67a5ffc89234c186e4c1dcff84934cb8fbe91dd0
Eric Smith <paytonrules@gmail.com>
2021-04-16 08:10:57 -0500

    Initial implementation of initialize
    This adds an initialize function on the the Game trait, and uses
    async_trait so that initialization can be async. It also means starting
    the game loop is also async. The game loop waits for initialization to
    complete before beginning it's famous update/draw pattern.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod engine;
 mod game;
 
 use engine::GameLoop;
-use game::{Sheet, WalkTheDog};
+use game::WalkTheDog;
 use wasm_bindgen::prelude::*;
 
 // This is like the `main` function, except for JavaScript.
@@ -13,21 +13,11 @@ pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
     browser::spawn_local(async move {
-        let json = browser::fetch_json("rhb.json")
+        let game = WalkTheDog::new();
+
+        GameLoop::start(game)
             .await
-            .expect("Could not fetch rhb.json");
-
-        let sheet: Sheet = json
-            .into_serde()
-            .expect("Could not convert rhb.json into a Sheet structure");
-
-        let image = engine::load_image("rhb.png")
-            .await
-            .expect("Could not load rhb.png");
-
-        let game = WalkTheDog::new(image, sheet);
-
-        GameLoop::start(game).expect("Could not start game loop");
+            .expect("Could not start game loop");
     });
 
     Ok(())
