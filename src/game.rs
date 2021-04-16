0fe2d6dea55693c8f52f88687e8c9d1b33dc74ff
Eric Smith <paytonrules@gmail.com>
2021-04-16 08:10:57 -0500

    Initial implementation of initialize
    This adds an initialize function on the the Game trait, and uses
    async_trait so that initialization can be async. It also means starting
    the game loop is also async. The game loop waits for initialization to
    complete before beginning it's famous update/draw pattern.

--- src/game.rs
+++ src/game.rs
@@ -1,9 +1,13 @@
-use std::collections::HashMap;
-
+use anyhow::{anyhow, Result};
+use async_trait::async_trait;
 use serde::Deserialize;
+use std::collections::HashMap;
 use web_sys::HtmlImageElement;
 
-use crate::engine::{Game, Rect, Renderer};
+use crate::{
+    browser,
+    engine::{self, Game, Rect, Renderer},
+};
 
 #[derive(Deserialize)]
 struct SheetRect {
@@ -24,22 +28,33 @@ pub struct Sheet {
 }
 
 pub struct WalkTheDog {
-    image: HtmlImageElement,
-    sheet: Sheet,
+    image: Option<HtmlImageElement>,
+    sheet: Option<Sheet>,
     frame: u8,
 }
 
 impl WalkTheDog {
-    pub fn new(image: HtmlImageElement, sheet: Sheet) -> Self {
+    pub fn new() -> Self {
         WalkTheDog {
-            image,
-            sheet,
+            image: None,
+            sheet: None,
             frame: 0,
         }
     }
 }
 
+#[async_trait(?Send)]
 impl Game for WalkTheDog {
+    async fn initialize(&mut self) -> Result<()> {
+        let json = browser::fetch_json("rhb.json")
+            .await
+            .map_err(|err| anyhow!("Could not load rhb.json {:#?}", err))?;
+
+        self.sheet = json.into_serde()?;
+        self.image = Some(engine::load_image("rhb.png").await?);
+        Ok(())
+    }
+
     fn update(&mut self) {
         if self.frame < 23 {
             self.frame += 1;
@@ -51,7 +66,12 @@ impl Game for WalkTheDog {
     fn draw(&self, renderer: &Renderer) {
         let current_sprite = (self.frame / 3) + 1;
         let frame_name = format!("Run ({}).png", current_sprite);
-        let sprite = self.sheet.frames.get(&frame_name).expect("Cell not found");
+
+        let sprite = self
+            .sheet
+            .as_ref()
+            .and_then(|sheet| sheet.frames.get(&frame_name))
+            .expect("Cell not found");
 
         renderer.clear(&Rect {
             x: 0.0,
@@ -59,20 +79,22 @@ impl Game for WalkTheDog {
             width: 600.0,
             height: 600.0,
         });
-        renderer.draw_image(
-            &self.image,
-            &Rect {
-                x: sprite.frame.x.into(),
-                y: sprite.frame.y.into(),
-                width: sprite.frame.w.into(),
-                height: sprite.frame.h.into(),
-            },
-            &Rect {
-                x: 300.0,
-                y: 300.0,
-                width: sprite.frame.w.into(),
-                height: sprite.frame.h.into(),
-            },
-        );
+        self.image.as_ref().map(|image| {
+            renderer.draw_image(
+                &image,
+                &Rect {
+                    x: sprite.frame.x.into(),
+                    y: sprite.frame.y.into(),
+                    width: sprite.frame.w.into(),
+                    height: sprite.frame.h.into(),
+                },
+                &Rect {
+                    x: 300.0,
+                    y: 300.0,
+                    width: sprite.frame.w.into(),
+                    height: sprite.frame.h.into(),
+                },
+            );
+        });
     }
 }
