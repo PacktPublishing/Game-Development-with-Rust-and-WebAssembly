98562a52084e400d066271906aa21b9a63918756
Eric Smith <paytonrules@gmail.com>
2021-04-17 10:57:23 -0500

    Remove unused namespaces

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,6 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use async_trait::async_trait;
-use futures::channel::mpsc::{channel, unbounded, Receiver, UnboundedReceiver};
+use futures::channel::mpsc::{unbounded, UnboundedReceiver};
 use serde::Deserialize;
 use std::collections::HashMap;
 use wasm_bindgen::{prelude::Closure, JsCast};
