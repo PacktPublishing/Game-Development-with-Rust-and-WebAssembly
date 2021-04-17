d101ead693094d7e21c774f02d050de829807f31
Eric Smith <paytonrules@gmail.com>
2021-04-17 10:57:33 -0500

    Use an Error when LoadImage fails
    Instead of JsValue. This did mean pulling in Closure to Engine, and I
    don't love that.

--- src/engine.rs
+++ src/engine.rs
@@ -3,7 +3,7 @@ use anyhow::{anyhow, Result};
 use async_trait::async_trait;
 use futures::channel::oneshot::channel;
 use std::{cell::RefCell, rc::Rc, sync::Mutex};
-use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
 pub struct Point {
@@ -51,8 +51,8 @@ impl Renderer {
 pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
     let image = browser::new_image()?;
 
-    let (success_tx, success_rx) = channel::<Result<(), JsValue>>();
-    let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+    let (complete_tx, complete_rx) = channel::<Result<()>>();
+    let success_tx = Rc::new(Mutex::new(Some(complete_tx)));
     let error_tx = Rc::clone(&success_tx);
     let success_callback = browser::closure_once(move || {
         if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
@@ -60,9 +60,9 @@ pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
         }
     });
 
-    let error_callback = browser::closure_once(move |err| {
+    let error_callback: Closure<dyn FnMut(JsValue)> = browser::closure_once(move |err| {
         if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-            error_tx.send(Err(err));
+            error_tx.send(Err(anyhow!("Error Loading Image: {:#?}", err)));
         }
     });
 
@@ -70,7 +70,7 @@ pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
     image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
     image.set_src(source);
 
-    success_rx.await?;
+    complete_rx.await?;
 
     Ok(image)
 }
