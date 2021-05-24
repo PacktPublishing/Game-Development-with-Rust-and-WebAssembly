458e584d85d800cd8eecf1e985d7691f40382da5
Eric Smith <paytonrules@gmail.com>
2021-05-24 07:02:18 -0500

    Delete duplicated RHB
    Now the RHB handles itself, and nothing in WalkTheDog is related to it.

--- src/game.rs
+++ src/game.rs
@@ -197,22 +197,12 @@ pub struct Sheet {
 }
 
 pub struct WalkTheDog {
-    image: Option<HtmlImageElement>,
-    sheet: Option<Sheet>,
-    frame: u8,
-    position: Point,
     rhb: Option<RedHatBoy>,
 }
 
 impl WalkTheDog {
     pub fn new() -> Self {
-        WalkTheDog {
-            image: None,
-            sheet: None,
-            frame: 0,
-            position: Point { x: 0, y: 0 },
-            rhb: None,
-        }
+        WalkTheDog { rhb: None }
     }
 }
 
@@ -220,49 +210,18 @@ impl WalkTheDog {
 impl Game for WalkTheDog {
     async fn initialize(&self) -> Result<Box<dyn Game>> {
         let json = browser::fetch_json("rhb.json").await?;
-        let sheet = json.into_serde()?;
-        let image = Some(engine::load_image("rhb.png").await?);
 
         let rhb = Some(RedHatBoy::new(
             json.into_serde::<Sheet>()?,
             engine::load_image("rhb.png").await?,
         ));
 
-        Ok(Box::new(WalkTheDog {
-            image,
-            sheet,
-            rhb,
-            frame: self.frame,
-            position: self.position,
-        }))
+        Ok(Box::new(WalkTheDog { rhb }))
     }
 
     fn update(&mut self, keystate: &KeyState) {
-        let mut velocity = Point { x: 0, y: 0 };
-        if keystate.is_pressed("ArrowDown") {
-            velocity.y += 3;
-        }
-
-        if keystate.is_pressed("ArrowUp") {
-            velocity.y -= 3;
-        }
-
         if keystate.is_pressed("ArrowRight") {
-            velocity.x += 3;
             self.rhb.as_mut().unwrap().run_right();
-        }
-
-        if keystate.is_pressed("ArrowLeft") {
-            velocity.x -= 3;
-        }
-
-        self.position.x += velocity.x;
-        self.position.y += velocity.y;
-
-        if self.frame < 23 {
-            self.frame += 1;
-        } else {
-            self.frame = 0;
         }
         self.rhb.as_mut().unwrap().update();
     }
@@ -273,32 +232,6 @@ impl Game for WalkTheDog {
             y: 0.0,
             width: 600.0,
             height: 600.0,
-        });
-
-        let current_sprite = (self.frame / 3) + 1;
-        let frame_name = format!("Run ({}).png", current_sprite);
-        let sprite = self
-            .sheet
-            .as_ref()
-            .and_then(|sheet| sheet.frames.get(&frame_name))
-            .expect("Cell not found");
-
-        self.image.as_ref().map(|image| {
-            renderer.draw_image(
-                &image,
-                &Rect {
-                    x: sprite.frame.x.into(),
-                    y: sprite.frame.y.into(),
-                    width: sprite.frame.w.into(),
-                    height: sprite.frame.h.into(),
-                },
-                &Rect {
-                    x: self.position.x.into(),
-                    y: self.position.y.into(),
-                    width: sprite.frame.w.into(),
-                    height: sprite.frame.h.into(),
-                },
-            );
         });
 
         self.rhb.as_ref().unwrap().draw(renderer);
