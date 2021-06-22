76bc01098a88bca82ae48ce0d55a36bd4342903c
Eric Smith <paytonrules@gmail.com>
2021-06-22 08:19:40 -0500

    Shadow game instead of initialized_game
    Mostly because the book doesn't require that whole change then.

--- src/engine.rs
+++ src/engine.rs
@@ -98,7 +98,7 @@ type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 impl GameLoop {
     pub async fn start(game: impl Game + 'static) -> Result<()> {
         let mut keyevent_receiver = prepare_input()?;
-        let mut initialized_game = game.initialize().await?;
+        let mut game = game.initialize().await?;
 
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
@@ -118,11 +118,11 @@ impl GameLoop {
 
             game_loop.accumulated_delta += (perf - game_loop.last_frame) as f32;
             while game_loop.accumulated_delta > FRAME_SIZE {
-                initialized_game.update(&keystate);
+                game.update(&keystate);
                 game_loop.accumulated_delta -= FRAME_SIZE;
             }
             game_loop.last_frame = perf;
-            initialized_game.draw(&renderer);
+            game.draw(&renderer);
 
             browser::request_animation_frame(f.borrow().as_ref().unwrap());
         }));
