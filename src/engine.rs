c6ef0390623018962dc0ab0922f16d74a50ee212
Eric Smith <paytonrules@gmail.com>
2021-06-29 22:25:51 -0500

    Draw the background from an Image struct

--- src/engine.rs
+++ src/engine.rs
@@ -70,6 +70,12 @@ impl Renderer {
             )
             .expect("Drawing is throwing exceptions! Unrecoverable error.");
     }
+
+    pub fn draw_entire_image(&self, image: &HtmlImageElement, position: &Point) {
+        self.context
+            .draw_image_with_html_image_element(image, position.x.into(), position.y.into())
+            .expect("Drawing is throwing exceptions! Unrecoverable error.");
+    }
 }
 
 pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
@@ -219,4 +225,19 @@ fn prepare_input() -> Result<UnboundedReceiver<KeyPress>> {
     onkeyup.forget();
 
     Ok(keyevent_receiver)
+}
+
+pub struct Image {
+    element: HtmlImageElement,
+    position: Point,
+}
+
+impl Image {
+    pub fn new(element: HtmlImageElement, position: Point) -> Self {
+        Self { element, position }
+    }
+
+    pub fn draw(&self, renderer: &Renderer) {
+        renderer.draw_entire_image(&self.element, &self.position)
+    }
 }
