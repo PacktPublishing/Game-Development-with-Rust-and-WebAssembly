be0076159d76a5ef3e658a35072aa1124c830cd0
Eric Smith <paytonrules@gmail.com>
2021-07-01 07:23:46 -0500

    Draw the stone that RHB will jump over

--- src/game.rs
+++ src/game.rs
@@ -297,6 +297,7 @@ pub enum WalkTheDog {
 struct Walk {
     boy: RedHatBoy,
     background: Image,
+    stone: Image,
 }
 
 impl WalkTheDog {
@@ -312,6 +313,7 @@ impl Game for WalkTheDog {
             WalkTheDog::Loading => {
                 let json = browser::fetch_json("rhb.json").await?;
                 let background = engine::load_image("BG.png").await?;
+                let stone = engine::load_image("Stone.png").await?;
 
                 let rhb = RedHatBoy::new(
                     json.into_serde::<Sheet>()?,
@@ -321,6 +323,7 @@ impl Game for WalkTheDog {
                 Ok(Box::new(WalkTheDog::Loaded(Walk {
                     boy: rhb,
                     background: Image::new(background, Point { x: 0, y: 0 }),
+                    stone: Image::new(stone, Point { x: 150, y: 546 }),
                 })))
             }
             WalkTheDog::Loaded(_) => Err(anyhow!("Error: Game is already initialized")),
@@ -356,6 +359,7 @@ impl Game for WalkTheDog {
         if let WalkTheDog::Loaded(walk) = self {
             walk.background.draw(renderer);
             walk.boy.draw(renderer);
+            walk.stone.draw(renderer);
         }
     }
 }
