6389ddda0777efbea164a5cdce07cc6c2e2899c1
Eric Smith <paytonrules@gmail.com>
2021-07-28 22:43:01 -0500

    Change RHB to be stationary
    He's stationary and the world moves.

--- src/engine.rs
+++ src/engine.rs
@@ -278,4 +278,8 @@ impl Image {
     pub fn bounding_box(&self) -> &Rect {
         &self.bounding_box
     }
+
+    pub fn move_horizontally(&mut self, distance: i16) {
+        self.position.x += distance;
+    }
 }
