61010f51e2294d2429e49236c41c49df9c1e5acc
Eric Smith <paytonrules@gmail.com>
2021-08-01 22:55:21 -0500

    Add a SpriteSheet struct

--- src/engine.rs
+++ src/engine.rs
@@ -317,3 +317,22 @@ pub struct Cell {
 pub struct Sheet {
     pub frames: HashMap<String, Cell>,
 }
+
+pub struct SpriteSheet {
+    sheet: Sheet,
+    image: HtmlImageElement,
+}
+
+impl SpriteSheet {
+    pub fn new(sheet: Sheet, image: HtmlImageElement) -> Self {
+        SpriteSheet { sheet, image }
+    }
+
+    pub fn cell(&self, name: &str) -> Option<&Cell> {
+        self.sheet.frames.get(name)
+    }
+
+    pub fn draw(&self, renderer: &Renderer, source: &Rect, destination: &Rect) {
+        renderer.draw_image(&self.image, source, destination);
+    }
+}
