f22181a53f1c64a6121f0d41e080fb40108d8629
Eric Smith <paytonrules@gmail.com>
2021-08-01 22:55:21 -0500

    Add a SpriteSheet struct

--- src/engine.rs
+++ src/engine.rs
@@ -321,3 +321,22 @@ impl Image {
         self.bounding_box.right()
     }
 }
+
+pub struct SpriteSheet {
+    sheet: Sheet,
+    image: HtmlImageElement,
+}
+
+impl SpriteSheet {
+    pub fn new(sheet: Sheet, image: HtmlImageElement) -> Self {
+        SpriteSheet { sheet, image }
+    }
+
+    pub fn cell(&self, name: &str) -> Option<&Cell> {
+        self.sheet.frames.get(name)
+    }
+
+    pub fn draw(&self, renderer: &Renderer, source: &Rect, destination: &Rect) {
+        renderer.draw_image(&self.image, source, destination);
+    }
+}
