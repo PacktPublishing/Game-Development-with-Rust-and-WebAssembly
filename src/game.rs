c3c38f5757ce85e0b4d9189374b4058e2bcf2f7c
Eric Smith <paytonrules@gmail.com>
2021-08-04 00:17:12 -0500

    Make Obstacle public
    It can now be used by segments, when that comes into existence

--- src/game.rs
+++ src/game.rs
@@ -12,7 +12,7 @@ use crate::{
 
 const HEIGHT: i16 = 600;
 
-trait Obstacle {
+pub trait Obstacle {
     fn check_intersection(&self, boy: &mut RedHatBoy);
     fn draw(&self, renderer: &Renderer);
     fn move_horizontally(&mut self, x: i16);
