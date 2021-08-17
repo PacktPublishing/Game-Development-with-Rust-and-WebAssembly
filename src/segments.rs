63cb0421e2a5ed3e91667756035098cf13001efa
Eric Smith <paytonrules@gmail.com>
2021-08-17 08:04:46 -0500

    Add a second segment

--- src/segments.rs
+++ src/segments.rs
@@ -73,3 +73,31 @@ pub fn rock_and_platform(
         )),
     ]
 }
+
+pub fn platform_and_rock(
+    stone: HtmlImageElement,
+    sprite_sheet: Rc<SpriteSheet>,
+    offset_x: i16,
+) -> Vec<Box<dyn Obstacle>> {
+    vec![
+        Box::new(Barrier::new(Image::new(
+            stone,
+            Point {
+                x: offset_x + 400,
+                y: 546,
+            },
+        ))),
+        Box::new(Platform::new(
+            sprite_sheet.clone(),
+            Point {
+                x: offset_x + 200,
+                y: 400,
+            },
+            vec![
+                "13.png".to_string(),
+                "14.png".to_string(),
+                "15.png".to_string(),
+            ],
+        )),
+    ]
+}
