436d2cc7605db211851e498d633993bea652fe46
Eric Smith <paytonrules@gmail.com>
2021-09-08 22:57:29 -0500

    Add fetch_array_buffer
    This gets an ArrayBuffer off a response and refactors to use common code
    with fetch_json.

--- src/browser.rs
+++ src/browser.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Result};
 use futures::Future;
+use js_sys::ArrayBuffer;
 use wasm_bindgen::{
     closure::WasmClosure, closure::WasmClosureFnOnce, prelude::Closure, JsCast, JsValue,
 };
@@ -58,18 +59,35 @@ pub async fn fetch_with_str(resource: &str) -> Result<JsValue> {
         .map_err(|err| anyhow!("error fetching {:#?}", err))
 }
 
-pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
-    let resp_value = fetch_with_str(json_path).await?;
-    let resp: Response = resp_value
+pub async fn fetch_response(resource: &str) -> Result<Response> {
+    fetch_with_str(resource)
+        .await?
         .dyn_into()
-        .map_err(|element| anyhow!("Error converting {:#?} to Response", element))?;
+        .map_err(|err| anyhow!("error converting fetch to Response {:#?}", err))
+}
+
+pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
+    let resp = fetch_response(json_path).await?;
 
     JsFuture::from(
         resp.json()
             .map_err(|err| anyhow!("Could not get JSON from response {:#?}", err))?,
     )
     .await
-    .map_err(|err| anyhow!("error fetching json {:#?}", err))
+    .map_err(|err| anyhow!("error fetching JSON {:#?}", err))
+}
+
+pub async fn fetch_array_buffer(resource: &str) -> Result<ArrayBuffer> {
+    let array_buffer = fetch_response(resource)
+        .await?
+        .array_buffer()
+        .map_err(|err| anyhow!("Error loading array buffer {:#?}", err))?;
+
+    JsFuture::from(array_buffer)
+        .await
+        .map_err(|err| anyhow!("Error converting array buffer into a future {:#?}", err))?
+        .dyn_into()
+        .map_err(|err| anyhow!("Error converting raw JSValue to ArrayBuffer {:#?}", err))
 }
 
 pub fn new_image() -> Result<HtmlImageElement> {
