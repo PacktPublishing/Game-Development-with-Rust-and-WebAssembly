a3560b76d2d0be169f9aaade214a64eacdbdaa66
Eric Smith <paytonrules@gmail.com>
2021-09-08 22:29:41 -0500

    Add play_sound function

--- src/sound.rs
+++ src/sound.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, Result};
-use web_sys::{AudioBufferSourceNode, AudioContext, AudioDestinationNode, AudioNode};
+use web_sys::{AudioBuffer, AudioBufferSourceNode, AudioContext, AudioDestinationNode, AudioNode};
 
 pub fn create_audio_context() -> Result<AudioContext> {
     AudioContext::new().map_err(|err| anyhow!("Could not create audio context: {:#?}", err))
@@ -17,4 +17,13 @@ fn connect_with_audio_node(
     buffer_source
         .connect_with_audio_node(&destination)
         .map_err(|err| anyhow!("Error connecting audio source to destination {:#?}", err))
+}
+
+pub fn play_sound(ctx: &AudioContext, buffer: &AudioBuffer) -> Result<()> {
+    let track_source = create_buffer_source(ctx)?;
+    track_source.set_buffer(Some(&buffer));
+    connect_with_audio_node(&track_source, &ctx.destination())?;
+    track_source
+        .start()
+        .map_err(|err| anyhow!("Could not start sound! {:#?}", err))
 }
