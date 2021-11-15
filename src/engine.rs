f126ee252cde1f4c8972b272f9f05cf672bac277
Eric Smith <paytonrules@gmail.com>
2021-11-15 06:59:02 -0600

    Show Frame Rate

--- src/engine.rs
+++ src/engine.rs
@@ -120,7 +120,8 @@ impl Renderer {
     pub fn draw_text(&self, text: &str, location: &Point) -> Result<()> {
         self.context.set_font("16pt serif");
         self.context
-            .fill_text(text, location.x.into(), location.y.into())?;
+            .fill_text(text, location.x.into(), location.y.into())
+            .map_err(|err| anyhow!("Error filling text {:#?}", err))?;
         Ok(())
     }
 }
@@ -190,16 +191,24 @@ impl GameLoop {
         let g = f.clone();
 
         let mut keystate = KeyState::new();
+
         *g.borrow_mut() = Some(browser::create_raf_closure(move |perf: f64| {
             process_input(&mut keystate, &mut keyevent_receiver);
 
-            game_loop.accumulated_delta += (perf - game_loop.last_frame) as f32;
+            let frame_time = perf - game_loop.last_frame;
+            game_loop.accumulated_delta += frame_time as f32;
             while game_loop.accumulated_delta > FRAME_SIZE {
                 game.update(&keystate);
                 game_loop.accumulated_delta -= FRAME_SIZE;
             }
             game_loop.last_frame = perf;
             game.draw(&renderer);
+
+            if cfg!(debug_assertions) {
+                unsafe {
+                    draw_frame_rate(&renderer, frame_time);
+                }
+            }
 
             browser::request_animation_frame(f.borrow().as_ref().unwrap()).unwrap();
         }));
@@ -211,6 +220,28 @@ impl GameLoop {
         )?;
         Ok(())
     }
+}
+
+unsafe fn draw_frame_rate(renderer: &Renderer, frame_time: f64) {
+    static mut FRAMES_COUNTED: i32 = 0;
+    static mut TOTAL_FRAME_TIME: f64 = 0.0;
+    static mut FRAME_RATE: i32 = 0;
+
+    FRAMES_COUNTED += 1;
+    TOTAL_FRAME_TIME += frame_time;
+
+    if TOTAL_FRAME_TIME > 1000.0 {
+        FRAME_RATE = FRAMES_COUNTED;
+        TOTAL_FRAME_TIME = 0.0;
+        FRAMES_COUNTED = 0;
+    }
+
+    if let Err(err) = renderer.draw_text(
+        &format!("Frame Rate {}", FRAME_RATE),
+        &Point { x: 400, y: 100 },
+    ) {
+        error!("Could not draw text {:#?}", err);
+    };
 }
 
 #[derive(Debug)]
